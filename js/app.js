68915b19f40173082b169f68c8a7abc93bd40e8f
Matheus Reis <reismatheus97@gmail.com>
2019-03-28 00:39:27 -0300

    Including filterering functionality

--- js/app.js
+++ js/app.js
@@ -3,24 +3,19 @@ var MapViewModel = function () {
     self.numberOfMarkers = ko.observable(0);
     self.markers = ko.observableArray();
     self.filterQuery = ko.observable();
-    self.filteredElements = ko.observableArray([{place_name: 'aaa'}]);
+    self.filteredElements = ko.observableArray();
 
     self.hasFilter = ko.computed(function() {
-
         if (self.filterQuery()) {
-            const filterQuery = self.filterQuery();
-            console.log("self >>", self)
-            console.log("filterQuery >>", filterQuery);
+            const filterQuery = self.filterQuery().toLowerCase();
             if (self.markers() && self.markers().length > 0) {
                 const markers = self.markers();
-                console.log("markers >>", markers)
                 let filteredMarkers = 
-                    markers.filter(it => it.place_name.includes(filterQuery));
+                    markers.filter(it => it.place_name.toLowerCase().includes(filterQuery));
                 self.filteredElements.removeAll();
                 self.filteredElements.push(...filteredMarkers);
             }
         }
-
         return !!self.filterQuery();
     }, self);
 
