a50172ff4655c5af34b9610f2c1011e75197de38
Matheus Reis <reismatheus97@gmail.com>
2019-05-02 01:29:17 -0300

    App beta version

--- js/ViewModel.js
+++ js/ViewModel.js
@@ -4,15 +4,15 @@ var MapViewModel = function () {
     self.markers = ko.observableArray();
     self.filterQuery = ko.observable();
     self.filteredElements = ko.observableArray();
-
-    self.venues = ko.observableArray();
-
+    self.venueMarkers = ko.observableArray();
+    self.activeMarker = ko.observable();
     self.markersMode = ko.observable(true);
     self.directions = ko.observableArray();
     self.origin = ko.observable();
     self.destination = ko.observable();
     self.travelProfile = ko.observable("driving");
     self.isDrawerOpen = ko.observable(true);
+    self.popups = ko.observableArray();
 
     self.addDirections = function (coordinate) {
         if (self.directions().length > 1) {
@@ -24,24 +24,36 @@ var MapViewModel = function () {
         
     }
 
-
     self.hasFilter = ko.computed(function() {
+        let nonFilteredMarkers = []
+        let filteredMarkers = []
         if (self.filterQuery()) {
+            
             const filterQuery = self.filterQuery().toLowerCase();
             if (self.markers() && self.markers().length > 0) {
-                const markers = self.markers();
-                let filteredMarkers = 
-                    markers.filter(it => it.place_name.toLowerCase().includes(filterQuery));
+                const markers = self.markers();  
+                markers.map(it => it.address_name.toLowerCase().includes(filterQuery) ? 
+                    filteredMarkers.push(it) :
+                    nonFilteredMarkers.push(it)
+                );
                 self.filteredElements.removeAll();
                 self.filteredElements.push(...filteredMarkers);
+                filteredMarkers.map(it => it.markerMapBox._element.style.display = "flex")
+                nonFilteredMarkers.map(it => it.markerMapBox._element.style.display = "none")
             }
+        } else {
+            self.markers().map(it => it.markerMapBox._element.style.display = "flex")
+            nonFilteredMarkers.map(it => it.markerMapBox._element.style.display = "flex")
         }
         return !!self.filterQuery();
     }, self);
 
-    self.addVenue = function (venue) {
-        self.venues.push(venue);
-        // self.numberOfVenues(self.numberOfVenues() + 1);
+    self.addVenueMarker = function (venueMarker) {
+        self.venueMarkers.push(venueMarker);
+    }
+
+    self.addPopups = function (popup) {
+        self.popups.push(popup);
     }
 
     self.addMarker = function (marker) {
